abbe28eac257ed30102e9388630460433bd81b15
pecole <kiyokuro96@gmail.com>
2023-11-26 16:21:58 +0900

    change indent color. add search file include hidden.

--- dotfiles/nvim/.config/nvim/lua/plugins/telescope.lua
+++ dotfiles/nvim/.config/nvim/lua/plugins/telescope.lua
@@ -1,21 +1,50 @@
 -- telescope --
 
 return {
-  'nvim-telescope/telescope-file-browser.nvim',
+  'nvim-telescope/telescope.nvim',
   dependencies = {
-    'nvim-telescope/telescope.nvim',
     'nvim-lua/plenary.nvim',
     'nvim-telescope/telescope-frecency.nvim'
   },
   keys = {
-    { '<C-p>', ':Telescope find_files<CR>',   mode = 'n' },
-    { '<C-g>', ':Telescope live_grep<CR>',    mode = 'n' },
-    { '<C-f>', ':Telescope frecency<CR>',     mode = 'n' },
-    { '<C-b>', ':Telescope file_browser<CR>', mode = 'n' }
+    { '<C-p>', ':Telescope find_files find_command=rg,--files,--hidden,--glob,!*.git<CR>', mode = 'n' },
+    { '<C-g>', ':Telescope live_grep<CR>',                                                 mode = 'n' },
+    { '<C-f>', ':Telescope frecency<CR>',                                                  mode = 'n' },
   },
   config = function()
     local telescope = require('telescope')
-    telescope.setup()
+    telescope.setup({
+      on_highlights = function(hl, c)
+        local prompt = "#2d3149"
+        hl.TelescopeNormal = {
+          bg = c.bg_dark,
+          fg = c.fg_dark,
+        }
+        hl.TelescopeBorder = {
+          bg = c.bg_dark,
+          fg = c.bg_dark,
+        }
+        hl.TelescopePromptNormal = {
+          bg = prompt,
+        }
+        hl.TelescopePromptBorder = {
+          bg = prompt,
+          fg = prompt,
+        }
+        hl.TelescopePromptTitle = {
+          bg = prompt,
+          fg = prompt,
+        }
+        hl.TelescopePreviewTitle = {
+          bg = c.bg_dark,
+          fg = c.bg_dark,
+        }
+        hl.TelescopeResultsTitle = {
+          bg = c.bg_dark,
+          fg = c.bg_dark,
+        }
+      end,
+    })
     telescope.load_extension('frecency')
   end
 }
