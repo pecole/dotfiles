50d397b9e6063992af16d64e215e76cc29b0fadc
pecole <kiyokuro96@gmail.com>
2024-03-26 04:55:05 +0900

    copilot tabキーの無効化されていない設定を修正

--- dotfiles/nvim/.config/nvim/lua/plugins/copilot.lua
+++ dotfiles/nvim/.config/nvim/lua/plugins/copilot.lua
@@ -1,10 +1,11 @@
 -- github copilot
 
+vim.g.copilot_no_tab_map = true
+
 return {
   'github/copilot.vim',
   event = 'VeryLazy',
   config = function()
     vim.keymap.set('i', '<C-j>', 'copilot#Accept("\\<CR>")', { expr = true, replace_keycodes = false })
-    vim.g.copilot_no_tab_map = true
   end
 }
