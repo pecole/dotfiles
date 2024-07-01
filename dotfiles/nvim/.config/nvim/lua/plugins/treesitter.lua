bfdbc3c681ada0e4e53560439313d670477c17ff
pecole <kiyokuro96@gmail.com>
2024-07-01 20:34:45 +0900

    wip

--- dotfiles/nvim/.config/nvim/lua/plugins/treesitter.lua
+++ dotfiles/nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -1,12 +1,26 @@
 return {
   'nvim-treesitter/nvim-treesitter',
   build = ':TSUpdate',
-  lazy = false,
+  event = 'VeryLazy',
   config = function()
-    require('nvim-treesitter.configs').setup({
-      hightlight = { enable = true, },
-      indent = { enable = true },
+    local config = require('nvim-treesitter.configs')
+    config.setup({
+      ensure_installed = { "lua" },
+      sync_install = true,
       auto_install = true,
+
+      highlight = { enable = true },
+      indent = { enable = true },
+
+      incremental_selection = {
+        enable = true,
+        keymaps = {
+          init_selection = "gnn",
+          node_incremental = "grn",
+          scope_incremental = "grc",
+          node_decremental = "grm",
+        },
+      },
     })
   end,
 }
