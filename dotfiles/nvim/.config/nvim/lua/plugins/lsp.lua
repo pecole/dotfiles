c1d655464375181c7f3bc1cceb58c5a0bd16ab47
pecole <kiyokuro96@gmail.com>
2024-07-17 13:30:17 +0900

    wip

--- dotfiles/nvim/.config/nvim/lua/plugins/lsp.lua
+++ dotfiles/nvim/.config/nvim/lua/plugins/lsp.lua
@@ -19,9 +19,9 @@ vim.api.nvim_create_autocmd('LspAttach', {
     -- See `:help vim.lsp.*` for documentation on any of the below functions
     vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, { buffer = ev.buf, desc = "Buffer - declaration" })
     vim.keymap.set('n', 'gd', vim.lsp.buf.definition, { buffer = ev.buf, desc = "Buffer - definition" })
-    vim.keymap.set('n', 'K', vim.lsp.buf.hover, { buffer = ev.buf, desc = "Buffer - hover" })
+    vim.keymap.set('n', 'gk', vim.lsp.buf.hover, { buffer = ev.buf, desc = "Buffer - hover" })
     vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, { buffer = ev.buf, desc = "Buffer - implementation" })
-    vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, { buffer = ev.buf, desc = "Buffer - signature help" })
+    vim.keymap.set('n', 'gh', vim.lsp.buf.signature_help, { buffer = ev.buf, desc = "Buffer - signature help" })
     vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder,
       { buffer = ev.buf, desc = "Buffer - add workspace folder" })
     vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder,
