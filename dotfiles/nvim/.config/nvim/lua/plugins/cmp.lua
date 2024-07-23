168c8e88025418be8b8e03f43e6bae69ba5e6d99
pecole <kiyokuro96@gmail.com>
2024-07-23 10:48:05 +0900

    wip

---
+++ dotfiles/nvim/.config/nvim/lua/plugins/cmp.lua
@@ -0,0 +1,41 @@
+return {
+  "L3MON4D3/LuaSnip",
+  dependencies = {
+    { "hrsh7th/nvim-cmp" },
+    { "hrsh7th/cmp-nvim-lsp" },
+    { "hrsh7th/cmp-buffer" },
+    { "saadparwaiz1/cmp_luasnip" },
+  },
+  event = { 'VeryLazy', 'BufReadPre', 'BufNewFile' },
+  config = function()
+    -- lspのハンドラーに設定
+    capabilities = require("cmp_nvim_lsp").default_capabilities()
+
+    -- lspの設定後に追加
+    vim.opt.completeopt = "menu,menuone,noselect"
+
+    local cmp = require "cmp"
+    cmp.setup({
+      snippet = {
+        expand = function(args)
+          require("luasnip").lsp_expand(args.body)
+        end,
+      },
+      mapping = cmp.mapping.preset.insert({
+        ["<C-p>"] = cmp.mapping.select_prev_item(),
+        ["<C-n>"] = cmp.mapping.select_next_item(),
+        ["<C-d>"] = cmp.mapping.scroll_docs(-4),
+        ["<C-f>"] = cmp.mapping.scroll_docs(4),
+        ["<C-Space>"] = cmp.mapping.complete(),
+        ["<C-e>"] = cmp.mapping.close(),
+        ["<CR>"] = cmp.mapping.confirm({ select = true }),
+      }),
+      sources = cmp.config.sources({
+        { name = "nvim_lsp" },
+        { name = "luasnip" },
+      }, {
+        { name = "buffer" },
+      }),
+    })
+  end
+}
