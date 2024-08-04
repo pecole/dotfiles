c97dd193a99f8364467ff571308fc43f9435ee99
pecole <kiyokuro96@gmail.com>
2024-08-04 10:38:45 +0900

    wip

--- dotfiles/nvim/.config/nvim/lua/plugins/barbar.lua
+++ dotfiles/nvim/.config/nvim/lua/plugins/barbar.lua
@@ -14,7 +14,6 @@ return {
     -- insert_at_start = true,
     -- …etc.
   },
-  version = '^1.0.0', -- optional: only update when a new 1.x version is released
   config = function()
     require('barbar').setup(
 
