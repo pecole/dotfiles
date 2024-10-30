de4c23f5cbff0a42b7f7d4359471157a2a5e82eb
pecole <komori@pecole.monster>
2024-10-30 12:11:49 +0900

    keymap修正

--- dotfiles/nvim/.config/nvim/lua/plugins/copilot-chat.lua
+++ dotfiles/nvim/.config/nvim/lua/plugins/copilot-chat.lua
@@ -91,6 +91,41 @@ return {
             end,
           },
         },
+        mappings = {
+          complete = {
+            detail = 'Use @<Tab> or /<Tab> for options.',
+            insert ='<Tab>',
+          },
+          close = {
+            normal = 'q',
+            insert = '<C-c>'
+          },
+          reset = {
+            normal ='<C-r>',
+            insert = '<C-r>'
+          },
+          submit_prompt = {
+            normal = '<CR>',
+            insert = '<C-s>'
+          },
+          accept_diff = {
+            normal = '<C-y>',
+            insert = '<C-y>'
+          },
+          yank_diff = {
+            normal = 'gy',
+            register = '"',
+          },
+          show_diff = {
+            normal = 'gd'
+          },
+          show_system_prompt = {
+            normal = 'gp'
+          },
+          show_user_selection = {
+            normal = 'gs'
+          },
+        },
       })
     end,
   },
